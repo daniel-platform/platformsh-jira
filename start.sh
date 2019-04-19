a9746654c784ed9e838edd8092ee2b08aed9c07d
daniel-platform <daniel@platform.sh>
2019-04-19 12:05:57 -0400

    Add SMTP properties at startup

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@
 export JAVA_HOME=${PLATFORM_APP_DIR}/jre
 export JIRA_LOGBASE=${PLATFORM_APP_DIR}/home
 export JIRA_FQDN=$(bin/routes PLATFORM_ROUTES jira | awk -F / '{print $3}' | uniq)
-export CATALINA_OPTS="-Dport.http=${PORT} -Dwork.dir=${PLATFORM_APP_DIR}/home/work -Dappbase.dir=${PLATFORM_APP_DIR}/home/webapps -Dlog.dir=${PLATFORM_APP_DIR}/home/logs -Dproxy.name=${JIRA_FQDN} -Dhttp.scheme=https -Dproxy.port=443"
+export CATALINA_OPTS="-Dport.http=${PORT} -Dwork.dir=${PLATFORM_APP_DIR}/home/work -Dappbase.dir=${PLATFORM_APP_DIR}/home/webapps -Dlog.dir=${PLATFORM_APP_DIR}/home/logs -Dproxy.name=${JIRA_FQDN} -Dhttp.scheme=https -Dproxy.port=443 -Dmail.smtp.host=${PLATFORM_SMTP_HOST} -Dmail.smtp.port=25"
 export CATALINA_TMPDIR=/tmp
 export CATALINA_OUT=/tmp/log/catalina.out
 export CATALINA_PID=${PLATFORM_APP_DIR}/home/work/catalina.pid
